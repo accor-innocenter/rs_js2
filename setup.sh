af29c8b6bf6f12585f23300e90e668953bc422e7
InnoCenter <innocenter@MacBook-Pro-7.local>
2019-08-28 18:41:36 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #/usr/bin/env bash -e
 
-npm install
-npm install hermes-javascript
-node action-rsjs2.js
+npm install && npm run build
+
+if [ ! -e "./config.ini" ]
+then
+    cp config.ini.default config.ini
+fi
